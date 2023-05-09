155dc2e0fa29c05c9c33aba81eba3327e2b4cf2a
ddalama1 <ddalama@tamu.edu>
2023-05-09 13:12:57 -0500

    new movie in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'John Wick', :rating => 'R',
+   :release_date => '5 May 2023'}
 ]
 
 more_movies.each do |movie|
